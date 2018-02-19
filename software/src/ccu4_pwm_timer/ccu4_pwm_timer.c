f1673dab7bc98d40635bd928342a1fbac238aa21
Olaf Lüke <olaf@tinkerforge.com>
2018-02-19 19:03:37 +0100

    Use correct shadow transfers, make sure that wie use slice number as integer instead of pointer if necessary, add proper indentation

--- software/src/ccu4_pwm_timer/ccu4_pwm_timer.c
+++ software/src/ccu4_pwm_timer/ccu4_pwm_timer.c
@@ -34,58 +34,48 @@ void count_init( const uint8_t ccu4_slice_number)
 {
 
 	XMC_CCU4_SLICE_COMPARE_CONFIG_t timer_config1 = {
-	.timer_mode            = XMC_CCU4_SLICE_TIMER_COUNT_MODE_EA,
-	.monoshot              = XMC_CCU4_SLICE_TIMER_REPEAT_MODE_REPEAT,
-	.shadow_xfer_clear     = false,
-	.dither_timer_period   = false,
-	.dither_duty_cycle     = false,
-	.prescaler_mode        = XMC_CCU4_SLICE_PRESCALER_MODE_NORMAL,
-	.mcm_enable            = false,
-	.prescaler_initval     = XMC_CCU4_SLICE_PRESCALER_1,
-	.float_limit           = XMC_CCU4_SLICE_PRESCALER_32768,
-	.dither_limit          = 0,
-	.passive_level         = XMC_CCU4_SLICE_OUTPUT_PASSIVE_LEVEL_LOW,
-	.timer_concatenation   = false
-};
-XMC_CCU4_SLICE_CompareInit(slice[ccu4_slice_number], &timer_config1);
+		.timer_mode            = XMC_CCU4_SLICE_TIMER_COUNT_MODE_EA,
+		.monoshot              = XMC_CCU4_SLICE_TIMER_REPEAT_MODE_REPEAT,
+		.shadow_xfer_clear     = false,
+		.dither_timer_period   = false,
+		.dither_duty_cycle     = false,
+		.prescaler_mode        = XMC_CCU4_SLICE_PRESCALER_MODE_NORMAL,
+		.mcm_enable            = false,
+		.prescaler_initval     = XMC_CCU4_SLICE_PRESCALER_1,
+		.float_limit           = XMC_CCU4_SLICE_PRESCALER_32768,
+		.dither_limit          = 0,
+		.passive_level         = XMC_CCU4_SLICE_OUTPUT_PASSIVE_LEVEL_LOW,
+		.timer_concatenation   = false
+	};
+	XMC_CCU4_SLICE_CompareInit(slice[ccu4_slice_number], &timer_config1);
 
 
-// Set the period/compare values
-XMC_CCU4_SLICE_SetTimerPeriodMatch(slice[ccu4_slice_number], 10);
-XMC_CCU4_SLICE_SetTimerCompareMatch(slice[ccu4_slice_number], 5);
-
-
-// Transfer configuration through shadow register
-XMC_CCU4_SetMultiChannelShadowTransferMode(CCU40, (uint32_t)XMC_CCU4_MULTI_CHANNEL_SHADOW_TRANSFER_SW_SLICE1);
-XMC_CCU4_SLICE_DisableCascadedShadowTransfer(slice[ccu4_slice_number]);
-
-XMC_CCU4_EnableShadowTransfer(CCU40, XMC_CCU4_SHADOW_TRANSFER_SLICE_1 | XMC_CCU4_SHADOW_TRANSFER_DITHER_SLICE_1 | XMC_CCU4_SHADOW_TRANSFER_PRESCALER_SLICE_1);
-
+	// Set the period/compare values
+	XMC_CCU4_SLICE_SetTimerPeriodMatch(slice[ccu4_slice_number], 10);
+	XMC_CCU4_SLICE_SetTimerCompareMatch(slice[ccu4_slice_number], 10);
 
 	XMC_CCU4_SLICE_EVENT_CONFIG_t event0_config0 = {
-			.mapped_input = XMC_CCU4_SLICE_INPUT_BB,
-			.edge         = XMC_CCU4_SLICE_EVENT_EDGE_SENSITIVITY_RISING_EDGE,
-			.level        = XMC_CCU4_SLICE_EVENT_LEVEL_SENSITIVITY_ACTIVE_HIGH,
-			.duration     = XMC_CCU4_SLICE_EVENT_FILTER_DISABLED
-		};
-		XMC_CCU4_SLICE_ConfigureEvent(slice[ccu4_slice_number], XMC_CCU4_SLICE_EVENT_0, &event0_config0);
-		XMC_CCU4_SLICE_CountConfig(slice[ccu4_slice_number], XMC_CCU4_SLICE_EVENT_0);
-/*
-		XMC_CCU4_SLICE_EnableEvent(slice[ccu4_slice_number], XMC_CCU4_SLICE_IRQ_ID_COMPARE_MATCH_UP);//Eingang für SLice 1
-		XMC_CCU4_SLICE_SetInterruptNode(slice[ccu4_slice_number], XMC_CCU4_SLICE_IRQ_ID_COMPARE_MATCH_UP, XMC_CCU4_SLICE_SR_ID_0); //auf
+		.mapped_input = XMC_CCU4_SLICE_INPUT_BB,
+		.edge         = XMC_CCU4_SLICE_EVENT_EDGE_SENSITIVITY_RISING_EDGE,
+		.level        = XMC_CCU4_SLICE_EVENT_LEVEL_SENSITIVITY_ACTIVE_HIGH,
+		.duration     = XMC_CCU4_SLICE_EVENT_FILTER_DISABLED
+	};
+	XMC_CCU4_SLICE_ConfigureEvent(slice[ccu4_slice_number], XMC_CCU4_SLICE_EVENT_0, &event0_config0);
+	XMC_CCU4_SLICE_CountConfig(slice[ccu4_slice_number], XMC_CCU4_SLICE_EVENT_0);
 
-		NVIC_EnableIRQ(8);
-		NVIC_SetPriority(8, 0);
-		XMC_SCU_SetInterruptControl(8, XMC_SCU_IRQCTRL_CCU40_SR0_IRQ8); 	//ändern
-*/
-		// Request shadow transfer for the slice 1
-		XMC_CCU4_EnableShadowTransfer(CCU40, XMC_CCU4_SHADOW_TRANSFER_SLICE_1 | XMC_CCU4_SHADOW_TRANSFER_PRESCALER_SLICE_1);
+	XMC_CCU4_SLICE_EnableEvent(slice[ccu4_slice_number], XMC_CCU4_SLICE_IRQ_ID_COMPARE_MATCH_UP);
+	XMC_CCU4_SLICE_SetInterruptNode(slice[ccu4_slice_number], XMC_CCU4_SLICE_IRQ_ID_COMPARE_MATCH_UP, XMC_CCU4_SLICE_SR_ID_0);
 
-		// Enable clock for slice 1
-		XMC_CCU4_EnableClock(CCU40,slice[ccu4_slice_number]);
-		XMC_CCU4_SLICE_StartTimer(slice[ccu4_slice_number]);
+	NVIC_EnableIRQ(8);
+	NVIC_SetPriority(8, 0);
+	XMC_SCU_SetInterruptControl(8, XMC_SCU_IRQCTRL_CCU40_SR0_IRQ8);
 
-
+	// Request shadow transfer for the slice 1
+    XMC_CCU4_EnableShadowTransfer(CCU40, (XMC_CCU4_SHADOW_TRANSFER_SLICE_0 << (ccu4_slice_number*4)) |
+    		                             (XMC_CCU4_SHADOW_TRANSFER_PRESCALER_SLICE_0 << (ccu4_slice_number*4)));
+	// Enable clock for slice 1
+	XMC_CCU4_EnableClock(CCU40, ccu4_slice_number);
+	XMC_CCU4_SLICE_StartTimer(slice[ccu4_slice_number]);
 }
 
 
